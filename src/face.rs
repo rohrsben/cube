8934e0e4f3aa58561226ed802915d4fe91cadb20
rohrsben <rohrsben@gmail.com>
2025-07-14 22:52:10 -0400

    misc: qualify more enums

--- src/face.rs
+++ src/face.rs
@@ -1,5 +1,4 @@
-use crate::tile_color::TileColor::{self, *};
-use Face::*;
+use crate::tile_color::TileColor;
 
 #[derive(PartialEq, Eq, Hash, Copy, Clone)]
 pub enum Face {
@@ -14,16 +13,16 @@ pub enum Face {
 impl Face {
     pub fn color(&self) -> TileColor {
         match self {
-            Front  => White,
-            Top    => Green,
-            Right  => Orange,
-            Left   => Red,
-            Bottom => Blue,
-            Back   => Yellow
+            Face::Front  => TileColor::White,
+            Face::Top    => TileColor::Green,
+            Face::Right  => TileColor::Orange,
+            Face::Left   => TileColor::Red,
+            Face::Bottom => TileColor::Blue,
+            Face::Back   => TileColor::Yellow
         }
     }
 
     pub fn as_vec() -> Vec<Face> {
-        vec![Front, Top, Right, Left, Bottom, Back]
+        vec![Face::Front, Face::Top, Face::Right, Face::Left, Face::Bottom, Face::Back]
     }
 }
